2a5eaf989337d548e9cbfa6344704509d42964f3
Sam Boesjes <534268@student.glu.nl>
2020-03-26 16:58:04 +0100

    index aangepast

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ include("config/connect.php");
 <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="../assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/main_style.css">
     <title>Home</title>
 </head>
 <body>
@@ -34,8 +35,8 @@ include("config/connect.php");
                                             <i class="fa fa-angle-down"></i>
                                         </a>
                                         <ul class="account_selection">
-                                            <li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
-                                            <li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
+                                            <li><a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
+                                            <li><a href="login.php"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
                                         </ul>
                                     </li>
                                     <ul class="navbar_user">
@@ -55,11 +56,6 @@ include("config/connect.php");
                         </div>
                     </div>
                 </div>
-
-
-        <div class="nav">
-            <a href="uitlog.php" class="btn logout-button"><b>Uitloggen</a>
-        </div>
     </header>
 </body>
 </html>
