bfc0cde84b9cb03a8d3b86906789553281ffbb95
Sam Boesjes <534268@student.glu.nl>
2020-03-26 12:29:06 +0100

    index aangepast

--- index.php
+++ index.php
@@ -1,34 +1,65 @@
 <?php
-    include("config/connect.php");
-    ?>
+include("config/connect.php");
+?>
 
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
+<html lang="en">
+<head>
+<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/css/style.css">
-        <title>Sams Webshop</title>
-    </head>
-    <body>
-        <div class="sidenav">
-            <div class="login-main-text">
-                <h2>Sams Webshop<br> Select page</h2>
-                <p>Kies hier wat u bent.</p>
-            </div>
-        </div>
-        <div class="main">
-            <div class="col-md-6 col-sm-12">
-                <div class="login-form">
-                    <form method="post" action="<?= BASEHREF;?>">
-                <form>
-                    <a href="<?php echo BASEHREF; ?>home_user.php" class="btn btn-black">User</a>
-                    <a href="<?php echo BASEHREF; ?>home_admin.php" class="btn btn-black">Admin</a>
-                </form>
+        <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <!-- Main Navigation -->
+
+        <div class="main_nav_container">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12 text-right">
+                                <div class="logo_container">
+                                    <a href="#">Scammable</a>
+                                </div>
+                                <nav class="navbar">
+                                    <ul class="navbar_menu">
+                                        <li><a href="index.php">home</a></li>
+                                        <li><a href="categories.php">shop</a></li>
+                                        <li><a href="contact.html">contact</a></li>
+                                    </ul>
+                                    <li class="account">
+                                        <a href="#">
+                                            MY ACCOUNT
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="account_selection">
+                                            <li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i>Sign In</a></li>
+                                            <li><a href="#"><i class="fa fa-user-plus" aria-hidden="true"></i>Register</a></li>
+                                        </ul>
+                                    </li>
+                                    <ul class="navbar_user">
+                                        <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+                                        <li class="checkout">
+                                            <a href="#">
+                                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <div class="hamburger_container">
+                                        <i class="fa fa-bars" aria-hidden="true"></i>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
+
+        <div class="nav">
+            <a href="uitlog.php" class="btn logout-button"><b>Uitloggen</a>
         </div>
-    
-    </body>
-    </html>
+    </header>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+    include("config/connect.php");
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Sams Webshop</title>
+    </head>
+    <body>
+        <div class="sidenav">
+            <div class="login-main-text">
+                <h2>Sams Webshop<br> Select page</h2>
+                <p>Kies hier wat u bent.</p>
+            </div>
+        </div>
+        <div class="main">
+            <div class="col-md-6 col-sm-12">
+                <div class="login-form">
+                    <form method="post" action="<?= BASEHREF;?>">
+                <form>
+                    <a href="<?php echo BASEHREF; ?>home_user.php" class="btn btn-black">User</a>
+                    <a href="<?php echo BASEHREF; ?>home_admin.php" class="btn btn-black">Admin</a>
+                </form>
+                </div>
+            </div>
+        </div>
+    
+    </body>
+    </html>
